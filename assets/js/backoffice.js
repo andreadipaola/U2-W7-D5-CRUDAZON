7ed2a28cb3ec01a511a8a62257312b1aea745111
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-03-17 16:44:52 +0100

    added some changes

---
+++ assets/js/backoffice.js
@@ -0,0 +1,89 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+const URL = id
+  ? "https://striveschool-api.herokuapp.com/api/product/" + id
+  : "https://striveschool-api.herokuapp.com/api/product/";
+
+const method = id ? "PUT" : "POST";
+
+const authKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MjBjMWY4MWI0MjAwMTM5YjI3YzUiLCJpYXQiOjE2NzkwNDA3MDUsImV4cCI6MTY4MDI1MDMwNX0._BPCWhG94dOIx4zEgf7S0GmYpDfEV82NWOd7SW4mX6s";
+
+window.onload = async () => {
+  if (id) {
+    document.getElementById("delete-btn").classList.remove("d-none");
+
+    try {
+      const res = await fetch(URL);
+      const productData = await res.json();
+      const { name, description, brand, imageUrl, price } = productData;
+
+      document.getElementById("product-name").value = name;
+      document.getElementById("product-description").value = description;
+      document.getElementById("product-brand").value = brand;
+      document.getElementById("product-image").value = imageUrl;
+      document.getElementById("product-price").value = price;
+
+      const submitBtn = document.getElementById("submit-btn");
+      submitBtn.classList.remove("btn-primary");
+      submitBtn.classList.add("btn-warning");
+      submitBtn.innerText = "Modifica";
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+const createProduct = async (event) => {
+  event.preventDefault();
+
+  const product = {
+    name: document.getElementById("product-name").value,
+    description: document.getElementById("product-description").value,
+    brand: document.getElementById("product-brand").value,
+    imageUrl: document.getElementById("product-image").value,
+    price: document.getElementById("product-price").value
+  };
+
+  try {
+    const res = await fetch(URL, {
+      method: method,
+      body: JSON.stringify(product),
+      headers: {
+        Authorization: `Bearer ${authKey}`,
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (res.ok) {
+      const createdObject = await res.json();
+      if (id) {
+        console.log(
+          "Pronotto # " + createdObject._id + ", modificato con successo"
+        );
+      } else {
+        console.log(
+          "Prodotto # " + createdObject._id + ", creato con successo"
+        );
+      }
+    } else {
+      throw new Error("C'è qualcosa che non va con la tua richiesta");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  const deleteProduct = async () => {
+    try {
+      console.log("DELETE");
+      const res = await fetch(URL, { method: "DELETE" });
+      const deletedObj = await res.json();
+
+      console.log("Hai eliminato il prodotto " + deletedObj.name);
+      window.location.assign("./index.html");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
---
+++ assets/js/index.js
@@ -0,0 +1,62 @@
+const URL = "https://striveschool-api.herokuapp.com/api/product/";
+const authKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0MjBjMWY4MWI0MjAwMTM5YjI3YzUiLCJpYXQiOjE2NzkwNDA3MDUsImV4cCI6MTY4MDI1MDMwNX0._BPCWhG94dOIx4zEgf7S0GmYpDfEV82NWOd7SW4mX6s";
+
+const loadProducts = async () => {
+  try {
+    const res = await fetch(URL, {
+      headers: {
+        Authorization: `Bearer ${authKey}`
+      }
+    });
+    console.log("res", res);
+
+    if (res.status === 400)
+      throw new Error("Hai scrtitto male qualcosa! Controlla la tua richiesta");
+    if (res.status === 404) throw new Error("Quello che cerchi non è qui!");
+    if (!res.ok) throw new Error("Non sappiamo cosa sia ma c'è un problema!");
+
+    const products = await res.json();
+    const row = document.querySelector("#row");
+    console.log(row);
+    products.forEach((product) => {
+      const column = document.createElement("div");
+      column.className = "col-md-4";
+
+      const card = document.createElement("div");
+      card.className = "card";
+
+      const cardImage = document.createElement("img");
+      cardImage.className = "card-img-top";
+      cardImage.setAttribute("src", product.imageUrl);
+
+      const cardBody = document.createElement("div");
+      cardBody.className = "card-body";
+
+      const cardTitle = document.createElement("h5");
+      cardTitle.className = "card-title";
+      cardTitle.innerText = product.name;
+
+      const cardText = document.createElement("p");
+      cardText.className = "card-text";
+      cardText.innerText = product.description;
+
+      const cardLink = document.createElement("a");
+      cardLink.className = "btn btn-primary";
+      cardImage.setAttribute("href", `details.html?id=${product._id}`);
+      cardLink.innerText = "Visualizza Prodotto";
+
+      cardBody.append(cardTitle, cardText, cardLink);
+      card.append(cardImage, cardBody);
+      column.append(card);
+      row.append(column);
+    });
+    console.log(products);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+window.onload = () => {
+  loadProducts();
+};
