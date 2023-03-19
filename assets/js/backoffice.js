845960062f1e939bcf98df8f83881354eceab080
Andrea Di paola <andr3a.dipaola@gmail.com>
2023-03-19 23:50:37 +0100

    added some changes

--- assets/js/backoffice.js
+++ assets/js/backoffice.js
@@ -31,8 +31,8 @@ window.onload = async () => {
       submitBtn.classList.remove("btn-primary");
       submitBtn.classList.add("btn-warning");
       submitBtn.innerText = "Modifica Prodotto";
-      submitBtn.setAttribute("data-bs-toggle", "modal");
-      submitBtn.setAttribute("data-bs-target", "#edit-modal-confirm");
+      // submitBtn.setAttribute("data-bs-toggle", "modal");
+      // submitBtn.setAttribute("data-bs-target", "#edit-modal-confirm");
     } catch (err) {
       console.log(err);
     }
@@ -99,3 +99,7 @@ const deleteProduct = async () => {
     console.log(err);
   }
 };
+
+function deleteForm() {
+  document.getElementById("product-form").reset();
+}
